2e4179177b852976301126fa38be4cdaf8290c09
Diego582 <penaomardiego@gmail.com>
2022-07-12 10:28:41 -0300

    TP3 PUNTO 2) HABILITAR JSON PARA LAS APIS REST - agregue  la linea para forzar la comunicacion a traves de JSON  en el archivo web

--- backend/basic/config/web.php
+++ backend/basic/config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'HFJE9i7OrCTURWliPn6jdmo4mmgsQtzx',
+            'parsers' => ['application/json' => \yii\web\JsonParser::class],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
