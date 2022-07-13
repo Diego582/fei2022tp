78f0167eff864a8a13073a94fd9c5741313f3094
Diego582 <penaomardiego@gmail.com>
2022-07-12 22:12:40 -0300

    TP4 PUNTO 5) AGREGADO DE LIBRERIA AXIOS - instala axios con npm, cree el archivo lo configure y lo importe al main para que lo instancie

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import axios from "axios";
+import VueAxios from "vue-axios";
+import custom_axios from "@/plugins/axios";
+
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios, custom_axios);
 
 new Vue({
   vuetify,
