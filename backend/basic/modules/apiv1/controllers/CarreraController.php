83f877326e66e7048999b29315f6248cc21a6325
Diego582 <penaomardiego@gmail.com>
2022-07-30 18:53:09 -0300

    TP FINAL - implementacion de CORS y cambio de la extension de controladores desde BAse Controller

--- backend/basic/modules/apiv1/controllers/CarreraController.php
+++ backend/basic/modules/apiv1/controllers/CarreraController.php
@@ -7,7 +7,7 @@ use yii\rest\ActiveController;
 /**
  * Default controller for the `apiv1` module
  */
-class CarreraController extends ActiveController
+class CarreraController extends BaseController
 {
     public $modelClass = 'app\modules\apiv1\models\Carrera';
 }
