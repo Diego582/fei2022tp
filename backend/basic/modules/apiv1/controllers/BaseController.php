b3bd81347543da2cc51345d8cf26bd123023dbd4
Diego582 <penaomardiego@gmail.com>
2023-01-12 22:22:06 -0300

    solucionado tema CORS

--- backend/basic/modules/apiv1/controllers/BaseController.php
+++ backend/basic/modules/apiv1/controllers/BaseController.php
@@ -2,38 +2,37 @@
 
 namespace app\modules\apiv1\controllers;
 
-
-use yii\filters\auth\HttpBearerAuth;
-use yii\filters\Cors;
 use yii\rest\ActiveController;
+use yii\web\Response;
+use yii\filters\ContentNegotiator;
+use yii\filters\Cors;
 
-
-/**
- * Default controller for the `apiv1` module
- */
 class BaseController extends ActiveController
 {
     public static function allowedDomains()
     {
-        return [$_SERVER["REMOTE_ADDR"], $_ENV['IP_REMOTE']];
+        return [
+            $_SERVER["REMOTE_ADDR"],
+            'http://localhost:8080',
+
+        ];
     }
 
 
-    public function behaviors()
+    function behaviors()
     {
-
-        /* $behaviors['authenticator']['authMethods'] = [HttpBearerAuth::class];
-        return $behaviors; */
         $behaviors = parent::behaviors();
+
         return array_merge($behaviors, [
-            'corsFilter'  => [
+            'corsFilter' => [
                 'class' => Cors::class,
-                'cors'  => [
-                    'Origin'                           => static::allowedDomains(),
-                    'Access-Control-Request-Method'    => ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-                    'Access-Control-Allow-Credentials' => true,
-                    'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
-                    'Access-Control-Allow-Headers' => ['authorization', 'X-Requested-With', 'content-type']
+                'cors' => [
+                    'Origin'                            => static::allowedDomains(),
+                    'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+                    'Access-Control-Allow-Credentials'  => true,
+                    'Access-Control-Max-Age'            => 3600,
+                    'Access-Control-Allow-Headers'      => ['authorization', 'X-Requested-With', 'content-type'],
+                    'Access-Control-Expose-Headers'     => ['X-Pagination-Current-Page', 'X-Pagination-Page-Count', 'X-Pagination-Total-Count'],
                 ],
             ],
         ]);
